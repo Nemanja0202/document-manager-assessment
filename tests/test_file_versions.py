b3f73de01c42f52344fec21b11159003d28a9a0c
Nemanja0202 <nemanja.savkic92@gmail.com>
2025-09-14 18:43:43 +0200

    Add unit tests for uploading files

--- tests/test_file_versions.py
+++ tests/test_file_versions.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from unittest.mock import MagicMock
 
 from pytest import raises
 from unittest import mock
@@ -172,3 +173,76 @@ class FileVersionRetrieveViewTests(APITestCase):
 
         with raises(Http404):
             FileVersionRetrieveView().get(self.request, self.file_url)
+
+
+def test_no_file_provided(user):
+    """
+    Tests that a ValidationError is raised when no file is provided to create()
+    """
+    file_url = "test/file.txt"
+    request = mock.MagicMock(user=user, data={"file_url": file_url})
+
+    with raises(ValidationError) as exc_info:
+        FileVersionViewSet().create(request)
+    assert exc_info.value.detail['detail'] == 'No file provided'
+
+
+@mock.patch("propylon_document_manager.file_versions.models.FileVersion.objects")
+@mock.patch("propylon_document_manager.file_versions.api.views.sha256")
+def test_same_file_hash_skips_saving(mock_sha256, mock_file_version_model, user):
+    """
+    Tests that no new files are saved if the hash is the same
+    """
+    file = MagicMock()
+    file_hash = "09fe6143410320a2d4a0bdfd782af8438e869fc531c5b06eab67c035649f3d78"
+    file_url = "test/file.txt"
+    mock_sha256.return_value.hexdigest.return_value = file_hash
+
+    latest_version_mock = MagicMock()
+    latest_version_mock.file_url = file_url
+    latest_version_mock.file_hash = file_hash
+    latest_version_mock.version_number = 1
+
+    mock_file_version_model.filter.return_value.order_by.return_value.first.return_value = latest_version_mock
+
+    request = MagicMock(user=user, data={"file_url": file_url, "file": file})
+    response = FileVersionViewSet().create(request)
+
+    assert response.status_code == status.HTTP_201_CREATED
+    assert response.data["version_number"] == 1
+    assert response.data["file_url"] == file_url
+    assert mock_file_version_model.create.not_called()
+
+
+@mock.patch("propylon_document_manager.file_versions.api.views.Path.mkdir")
+@mock.patch("propylon_document_manager.file_versions.api.views.open", new_callable=mock.mock_open)
+@mock.patch("propylon_document_manager.file_versions.api.views.get_directories", return_value=("media/path", "file.txt"))
+@mock.patch("propylon_document_manager.file_versions.models.FileVersion.objects")
+@mock.patch("propylon_document_manager.file_versions.api.views.sha256")
+def test_new_file_saves_correctly(mock_sha256, mock_file_version_model, mock_get_dirs, mock_open_file, mock_mkdir, user):
+    """
+    Tests that files are created
+    """
+    file = MagicMock()
+    file_hash = "09fe6143410320a2d4a0bdfd782af8438e869fc531c5b06eab67c035649f3d78"
+    file_url = "test/file.txt"
+    mock_sha256.return_value.hexdigest.return_value = file_hash
+
+    # No latest version found
+    mock_file_version_model.filter.return_value.order_by.return_value.first.return_value = None
+    # No existing file with the same hash
+    mock_file_version_model.filter.return_value.first.return_value = None
+
+    mock_file_version_model.create.return_value = mock.Mock(
+        file_url=file_url,
+        version_number=0
+    )
+
+    request = MagicMock(user=user, data={"file_url": file_url, "file": file})
+    response = FileVersionViewSet().create(request)
+
+    assert response.status_code == status.HTTP_201_CREATED
+    assert response.data["version_number"] == 0
+    assert mock_mkdir.called
+    mock_open_file.assert_called_once_with(os.path.join("media/path", file_hash), "wb")
+    mock_file_version_model.create.assert_called_once()
